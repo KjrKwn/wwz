5c81ea2d5640db2821f5d4c8ea65fa1920852703
KAWANAKojiro-no-MacBook-Pro <kawana@utap.phys.s.u-tokyo.ac.jp>
2019-01-17 11:02:54 +0900

    tochuu: for python setup.py install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name="wwz",
+        version="0.9.0",
+        description="weighted wavelet Z-transform(WWZ)",
+        author='Kojiro Kawana',
+        author_email='kojiro.kawana@gmail.com',
+        install_requires=["numpy","cypes"],
+        )
